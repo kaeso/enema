30fc62c95ea0ca14665e80687ab006a901129c2b
shsmoker@gmail.com <shsmoker@gmail.com@059f5fd4-bcd4-6b54-2a9d-ce7c43e78913>
2012-04-20 11:50:53 +0000

    New, friendly Query Editor interface ;)

--- trunk/core/e_const.py
+++ trunk/core/e_const.py
@@ -1,7 +1,7 @@
 #Constants
-VERSION = "1.70"
+VERSION = "1.71"
 CONFIG_PATH = "settings/enema.ini"
-QSTRINGS_DEFAULT_PATH = "settings/qstrings.ini"
-QSTRINGS_CUSTOM_PATH = "settings/qstrings_custom.ini"
+QSTRINGS_DEFAULT_PATH = "settings/qstrings/default.ini"
+QSTRINGS_CUSTOM_PATH = "settings/qstrings/custom.ini"
 DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0.2) Gecko/20100101 Firefox/6.0.2'
 QUOTED_CONTENT = False
